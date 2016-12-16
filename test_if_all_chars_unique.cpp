6563e31194048ad38ef89b33f5d2329f94c32bed
NK <nattykohn@gmail.com>
2016-12-16 21:25:34 +0200

    Create test_if_all_chars_unique.cpp

---
+++ test_if_all_chars_unique.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+bool isAllCharUnique(string stringToEval)
+{
+	int dictBitMask = 0;
+	for (int i = 0; i < stringToEval.size(); ++i) {
+		int val = stringToEval[i] - 'a';
+		if (dictBitMask & 1 << val) {
+			return false;
+		}
+		dictBitMask |= 1 << val;
+	}
+	return true;
+}
+
+
+int main()
+{
+
+	string myStr;
+	cout << "This program will test your string if all characters are unique.\n Please enter your string and type enter: \n";
+	while (cin >> myStr) {
+		
+		cout << (isAllCharUnique(myStr) ? "Unique\n\n" : "Not unique\n\n");
+		cout << "You can try again.\n Please enter your string and type enter: \n";
+	}		
+	return 0;
+}
